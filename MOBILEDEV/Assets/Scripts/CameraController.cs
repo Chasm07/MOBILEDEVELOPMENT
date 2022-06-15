ba6854bce48ef25e46625e1e69cf88245b87581a
Chasm07 <87064717+Chasm07@users.noreply.github.com>
2022-06-16 03:36:37 +0800

    fixed camera position and player

---
+++ MOBILEDEV/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    //Follow Player
+    [SerializeField] private Transform player;
+
+    private void Update()
+    {
+        //
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
