599e6f8147ece20c793efacf0eebd6fdfad428d8
Chasm07 <87064717+Chasm07@users.noreply.github.com>
2022-07-13 01:24:45 +0800

    added levels/fix minor bugs

--- MOBILEDEV/Assets/Scripts/CameraController.cs
+++ MOBILEDEV/Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
+    private float currentPosY;
 
     //Follow Player
     [SerializeField] private Transform player;
@@ -15,10 +16,12 @@ public class CameraController : MonoBehaviour
     {
         //
         //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+
     }
     public void MoveToNewRoom(Transform _newRoom)
     {
         currentPosX = _newRoom.position.x;
+        currentPosY = _newRoom.position.y + 0.01f;
     }
 }
