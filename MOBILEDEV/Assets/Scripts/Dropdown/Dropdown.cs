4b874b4672e663214fdac97902226bf0d81f422b
Chasm07 <87064717+Chasm07@users.noreply.github.com>
2022-07-15 12:42:51 +0800

    dropdown missions

---
+++ MOBILEDEV/Assets/Scripts/Dropdown/Dropdown.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dropdown : MonoBehaviour
+{
+
+    public TextMeshProUGUI output;
+
+    public void HandleInputData(int val)
+    {
+        if (val == 0)
+        {
+            output.text = "";
+        }
+        if (val == 1)
+        {
+            output.text = "Get at least one pen";
+        }
+        if (val == 2)
+        {
+            output.text = "Finish the game under 1 minute";
+        }
+    }
+}
