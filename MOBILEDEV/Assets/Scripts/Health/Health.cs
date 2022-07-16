726c42cee67537c998d1ad2af4d2c8b97d9fddd0
Chasm07 <87064717+Chasm07@users.noreply.github.com>
2022-07-16 23:27:51 +0800

    fix bug

--- MOBILEDEV/Assets/Scripts/Health/Health.cs
+++ MOBILEDEV/Assets/Scripts/Health/Health.cs
@@ -12,12 +12,22 @@ public class Health : MonoBehaviour
     private GameManager gameManager;
 
     public float currentHealth { get; private set; }
-
+    private Animator anim;
     private bool dead;
+
+    [SerializeField] 
+    private float iFramesDuration;
+
+    [SerializeField]
+    private float numberOfFlashes;
+
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     // Start is called before the first frame update
     public void TakeDamage(float _damage)
@@ -28,12 +38,15 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             //iframe
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
 
             if (!dead)
             {
+                anim.SetTrigger("die");
                 GetComponent<Player>().enabled = false;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
                 dead = true;
@@ -45,5 +58,18 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for(int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(1);
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
